6c2323966a7a7e3a455bc2a4da8890693440a2e4
chiziivictor <chizivwokoma@gmail.com>
2022-11-26 22:33:58 +0100

    feat: add button component

---
+++ src/components/ui/button/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { cva } from "class-variance-authority";
+import type { VariantProps } from "class-variance-authority";
+
+type Props = VariantProps<typeof buttonStyles> & {
+  children: React.ReactNode;
+};
+
+const buttonStyles = cva(
+  "font-semibold border rounded-2xl transition-all ease-linear duration-300 text-white border-none",
+  {
+    variants: {
+      intent: {
+        primary: "bg-gradient-to-r from-liver to-blue hover:bg-gradient-to-br",
+        secondary: "bg-liver border border-liver hover:bg-gray-100",
+        link: "",
+      },
+      size: {
+        small: ["text-sm", "py-1", "px-7"],
+        medium: ["text-base", "py-2", "px-14"],
+      },
+    },
+    defaultVariants: {
+      intent: "primary",
+      size: "medium",
+    },
+  }
+);
+
+const Button = ({ children, intent, size }: Props) => {
+  return <button className={buttonStyles({ intent, size })}>{children}</button>;
+};
+
+export default Button;
