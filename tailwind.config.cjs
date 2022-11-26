3a3b78607d50aaf5bbe60e0020827a113598ba8d
chiziivictor <chizivwokoma@gmail.com>
2022-11-26 21:55:10 +0100

    feat: add custom colors to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: "#c9e9e8",
+        teal: "#929dca",
+        blue: "#006b9b",
+        dark: "#11151c",
+        liver: "#483C46",
+      },
+    },
   },
   plugins: [],
 };
