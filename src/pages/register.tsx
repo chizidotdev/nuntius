908122a18894910dff0843607c193005fb65dd2f
chiziivictor <chizivwokoma@gmail.com>
2022-11-27 21:14:41 +0100

    feat: implement register form on client

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,11 +3,18 @@ import Button from "@ui/button";
 import Input from "@ui/input";
 import Layout from "@ui/layout";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { trpc } from "src/utils/trpc";
 
 const Register = () => {
+  const { mutate } = trpc.user.register.useMutation();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    const newUser = mutate({ username, password });
+    console.log({ newUser });
   };
 
   return (
@@ -19,11 +26,18 @@ const Register = () => {
       </p>
 
       <form onSubmit={handleSubmit} className="mt-5 flex w-full flex-col gap-5">
-        <Input name="username" placeholder="Enter your username" />
+        <Input
+          name="username"
+          placeholder="Enter your username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
         <Input
           name="password"
           placeholder="Enter your password"
           type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
         <Button>Register Account</Button>
       </form>
