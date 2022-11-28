0066d3789dc25e85db12e224d715cae9dbd2a2b3
chiziivictor <chizivwokoma@gmail.com>
2022-11-28 20:18:32 +0100

    feat: update meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,6 +15,31 @@ export default function Document() {
           rel="stylesheet"
         />
         <link rel="shortcut icon" href="/bubble-chat.png" type="image/x-icon" />
+        <meta
+          name="description"
+          content="Nuntius is an interactive anonymous messaging site with a dare game. Create your Profile Link and Send it to all your contacts to check what your friends will say to you"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://nuntius.chizi.dev/login" />
+        <meta property="og:title" content="Login - Nuntius" />
+        <meta
+          property="og:description"
+          content="Nuntius is an interactive anonymous messaging site with a dare game. Create your Profile Link and Send it to all your contacts to check what your friends will say to you"
+        />
+        <meta property="og:image" content="" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://nuntius.chizi.dev/login"
+        />
+        <meta property="twitter:title" content="Login - Nuntius" />
+        <meta
+          property="twitter:description"
+          content="Nuntius is an interactive anonymous messaging site with a dare game. Create your Profile Link and Send it to all your contacts to check what your friends will say to you"
+        />
+        <meta property="twitter:image" content="" />
       </Head>
       <body>
         <Main />
