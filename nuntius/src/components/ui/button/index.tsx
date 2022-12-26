96b683b6ab7c59d961b495e70401d814de73f4f0
chizidotdev <chizivwokoma@gmail.com>
2022-12-26 03:00:31 +0100

    changeset: initialize turborepo

---
+++ nuntius/src/components/ui/button/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { cva } from "class-variance-authority";
+import type { VariantProps } from "class-variance-authority";
+
+type Props = VariantProps<typeof buttonStyles> & {
+  children: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  type?: "button" | "submit" | "reset";
+};
+
+const buttonStyles = cva(
+  "font-semibold border rounded-2xl transition-all ease-linear duration-300 border-none",
+  {
+    variants: {
+      intent: {
+        primary:
+          "bg-gradient-to-r from-liver to-blue hover:bg-gradient-to-br text-white",
+        secondary: "bg-liver border border-liver hover:bg-gray-100 text-white",
+        link: "text-liver underline px-0 underline-offset-2",
+      },
+      size: {
+        small: ["text-sm", "py-1", "px-7"],
+        medium: ["text-base", "py-2", "px-14"],
+      },
+    },
+    defaultVariants: {
+      intent: "primary",
+      size: "medium",
+    },
+  }
+);
+
+const Button = ({ children, intent, size, ...props }: Props) => {
+  return (
+    <button className={buttonStyles({ intent, size })} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ packages/ui/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import Button from "./button";
+import Input from "./input";
+import Textarea from "./textarea";
+import Layout from "./layout";
+import Container from "./container";
+
+export { Button, Input, Textarea, Layout, Container };
